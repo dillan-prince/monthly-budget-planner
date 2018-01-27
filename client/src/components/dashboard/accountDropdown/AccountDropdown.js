a573e672ff040c5bdb6ed75baea8201b555d3f27
Dillan Prince <dillanprince@gmail.com>
2018-01-26 22:53:03 -0600

    SelectField is a controlled component...

--- client/src/components/dashboard/accountDropdown/AccountDropdown.js
+++ client/src/components/dashboard/accountDropdown/AccountDropdown.js
@@ -3,12 +3,22 @@ import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 
 class AccountDropdown extends Component {
+  state = {
+    selectedAccount: this.props.accounts
+      ? this.props.accounts.find((account) => account.isDefault)._id
+      : null
+  };
+
   render() {
     if (this.props.accounts && this.props.accounts.length) {
       const defaultAccount = this.props.accounts.find((account) => account.isDefault);
 
       return (
-        <SelectField value={defaultAccount._id} disabled={this.props.accounts.length === 1}>
+        <SelectField
+          value={this.state.selectedAccount || defaultAccount._id}
+          onChange={this.handleChange.bind(this)}
+          disabled={this.props.accounts.length === 1}
+        >
           {this.renderAccountDropdownOptions()}
         </SelectField>
       );
@@ -21,6 +31,10 @@ class AccountDropdown extends Component {
     return this.props.accounts.map((account) => {
       return <MenuItem key={account._id} value={account._id} primaryText={account.name} />;
     });
+  }
+
+  handleChange(event, index, value) {
+    this.setState({ selectedAccount: value });
   }
 }
 
