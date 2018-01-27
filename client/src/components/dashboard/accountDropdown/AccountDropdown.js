66eec007c8338fa6c0bb86ba3c1a5b4eccf9d6d9
Dillan Prince <dillanprince@gmail.com>
2018-01-27 00:07:28 -0600

    use lifecycle hook to update selected account when new account is created

--- client/src/components/dashboard/accountDropdown/AccountDropdown.js
+++ client/src/components/dashboard/accountDropdown/AccountDropdown.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 
 class AccountDropdown extends Component {
-  state = {
-    selectedAccount: this.props.accounts
-      ? this.props.accounts.find((account) => account.isDefault)._id
-      : null
-  };
+  state = { selectedAccount: null };
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.accounts) {
+      if (this.props.accounts) {
+        if (newProps.accounts.length > this.props.accounts.length) {
+          const newAccount = newProps.accounts.find((n) => {
+            return this.props.accounts.every((o) => o._id !== n._id);
+          });
+
+          this.setState({ selectedAccount: newAccount._id });
+        }
+      } else {
+        const defaultAccount = newProps.accounts.find((account) => account.isDefault);
+        this.setState({ selectedAccount: defaultAccount._id });
+      }
+    }
+  }
 
   render() {
     if (this.props.accounts && this.props.accounts.length) {
-      const defaultAccount = this.props.accounts.find((account) => account.isDefault);
-
       return (
         <SelectField
-          value={this.state.selectedAccount || defaultAccount._id}
+          value={this.state.selectedAccount}
           onChange={this.handleChange.bind(this)}
           disabled={this.props.accounts.length === 1}
         >
@@ -35,7 +47,13 @@ class AccountDropdown extends Component {
 
   handleChange(event, index, value) {
     this.setState({ selectedAccount: value });
+
+    // TODO: emit event indicating a new account was selected.
   }
 }
 
-export default AccountDropdown;
+function mapStateToProps({ accounts }) {
+  return { accounts };
+}
+
+export default connect(mapStateToProps)(AccountDropdown);
